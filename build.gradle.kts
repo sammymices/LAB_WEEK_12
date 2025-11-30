3763acd61f8ce452fb505a3b47f88e86746744af
Sammy <sammysamgar56@gmail.com>
2025-11-30 18:23:40 +0700

    Commit No. 1 – Init template

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+buildscript {
+    extra.apply {
+        set("moshi_version", "1.15.1")
+    }
+}
